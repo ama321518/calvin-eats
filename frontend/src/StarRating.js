19edf24d11cd3bc40d5b70a1f5fd214a7dd4852b
Ama Yeboah <apyeboah004@gmail.com>
2026-07-19 01:08:41 -0400

    connect star rating to backend

--- frontend/src/StarRating.js
+++ frontend/src/StarRating.js
@@ -4,12 +4,21 @@ function StarRating({ mealName }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
+  const submitRating = (star) => {
+    setRating(star);
+    fetch(`http://127.0.0.1:8000/api/ratings?meal_name=${mealName}&rating=${star}&comment=`, {
+      method: 'POST'
+    })
+    .then(res => res.json())
+    .then(data => console.log(data));
+  };
+
   return (
     <div>
       {[1, 2, 3, 4, 5].map((star) => (
         <span
           key={star}
-          onClick={() => setRating(star)}
+          onClick={() => submitRating(star)}
           onMouseEnter={() => setHover(star)}
           onMouseLeave={() => setHover(0)}
           style={{ 
